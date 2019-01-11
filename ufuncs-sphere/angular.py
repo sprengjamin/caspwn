bf4056a72baf990a0a187ebd3863cbdc2d00e7e8
Benjamin <benjamin.spreng@physik.uni-augsburg.de>
2019-01-11 14:22:04 +0100

    add angular script and corresponding tests

---
+++ ufuncs-sphere/angular.py
@@ -0,0 +1,267 @@
+r"""Exponentially scaled angular functions.
+
+This module provides functions for computing the angular functions
+:math:`\pi_\ell(z)` and :math:`\pi_\ell(z)`. However, for convenience, we compute
+the functions
+
+.. math::
+    \begin{aligned}
+    p_\ell(\cosh x) &\equiv \frac{2\ell+1}{\ell(\ell+1)} \pi_\ell(\cosh x) = \frac{2\ell+1}{\sinh x} P^{-1}_\ell (\cosh x) \,,\\
+    t_\ell(\cosh x) &\equiv \frac{2\ell+1}{\ell(\ell+1)} \tau_\ell(\cosh x) = -\cosh x\, p_\ell(\cosh x) + (2\ell+1)P_\ell(\cosh x)
+    \end{aligned}
+
+and to prevent under/overflow it is important to consider the exponentially scaled functions
+
+.. math::
+    \begin{aligned}
+    \tilde p_\ell(x) &= p_\ell(x) e^{-(\ell+1/2)\mathrm{arccosh}\vert x\vert}\,,\\
+    \tilde t_\ell(x) &= t_\ell(x) e^{-(\ell+1/2)\mathrm{arccosh}\vert x\vert}\,.
+    \end{aligned}
+
+
+"""
+import numpy as np
+from numba import jit
+from bessel import Ine
+from legendre import Ple_asymptotics
+
+@jit("float64(int64, float64)", nopython=True)
+def _cf(m, x):
+    """
+    2*n+1 is start value
+    """
+    x2 = x*x
+    num = 2*m+3
+    denom = 2*m+3 + x2/(2*m+1)
+    ratio = 1/(2*m+1)*num/denom
+    ratio_last = 0.
+    n = m+2
+    while(True):
+        num = 1+2*n+x2/num
+        denom = 1+2*n+x2/denom
+        ratio *= num/denom
+        if ratio == ratio_last:
+            return ratio
+        ratio_last = ratio
+        n += 1
+
+@jit("float64(float64)", nopython=True)
+def _c1(x):
+    if x < 1.:
+        y = _cf(1, x)
+        return -y/8.
+    else:
+        return (1 - x/np.tanh(x))/(8.*x**2)
+
+@jit("float64(float64)", nopython=True)
+def _c2(x):
+    if x < 1.:
+        y = _cf(2, x)
+        return (-3 + 8*y*(3 + x**2*y))/(384.*(3 + x**2*y)**2)
+    else:
+        y = x/np.tanh(x)
+        return (8*x**2 - 3*(-7 + 6*y + y**2))/(384.*x**4) 
+
+@jit("float64(float64)", nopython=True)
+def _c3(x):
+    if x < 1.:
+        y = _cf(3, x)
+        return (225*(1 - 8*y) - x**2*(-40 + 3*y*(75 + y*(240 + x**2*(23 + (24 + x**2)*y)))))/(3072.*(15 + x**2*(1 + 3*y))**3)
+    else:
+        y = x/np.tanh(x)
+        return (40*x**2 - 3*(-33 + 27*y + 5*y**2 + y**3))/(3072.*x**6) 
+
+@jit("float64(float64)", nopython=True)
+def _c4(x):
+    if x < 1.:
+        y = _cf(4, x)
+        return (64*x**12*y**4 + 1157625*(-41 + 384*y) + 73500*x**2*(-181 + 9*y*(191 + 288*y)) + 80*x**10*y**2*(3 + y*(68 + 321*y)) + 350*x**4*(-5293 + 3*y*(10994 + 81*y*(641 + 320*y))) + 100*x**6*(-291 + y*(-1589 + 9*y*(5795 + 3*y*(2699 + 480*y)))) + 15*x**8*(-15 + y*(-124 + y*(3238 + y*(43364 + 21105*y)))))/(1.47456e6*(105 + x**4*y + 5*x**2*(2 + 3*y))**4) 
+    else:
+        y = x/np.tanh(x)
+        return (64*x**4 + 240*x**2*(55 + y**2) - 45*(-715 + 572*y + 110*y**2 + 28*y**3 + 5*y**4))/(1.47456e6*x**8)
+
+@jit("float64(float64)", nopython=True)
+def _c5(x):
+    if x < 1.:
+        y = _cf(5, x)
+        return -(105*x**20*y**5 + 265831216875*(-23 + 256*y) + 5*x**18*y**3*(2 + 5*y)*(-16 + 575*y) + 843908625*x**2*(-4426 + 35*y*(1037 + 1024*y)) + 281302875*x**4*(-1640 + y*(17183 + 10*y*(4937 + 1792*y))) + 595350*x**6*(-87376 + 5*y*(159064 + 7*y*(114732 + 5*y*(21851 + 3584*y)))) + x**16*y*(64 + 5*y*(-1072 + y*(19416 + 25*y*(6467 + 9314*y)))) + 33075*x**8*(-72320 + y*(319544 + 5*y*(1376594 + y*(2351546 + 35*y*(28379 + 1792*y))))) + 315*x**10*(-159072 + 5*y*(-46832 + 5*y*(953804 + 35*y*(127002 + y*(98842 + 16051*y))))) + 6*x**14*(128 + 5*y*(-2368 + y*(15296 + 5*y*(107096 + 7*y*(58516 + 52555*y))))) + 35*x**12*(-11392 + y*(-169136 + 25*y*(126584 + y*(1247390 + y*(2623270 + 806183*y))))))/(3.93216e6*(945 + 105*x**2*(1 + y) + x**4*(1 + 10*y))**5)
+    else:
+        y = x/np.tanh(x)
+        return (-64*x**4*(-3 + y) + 80*x**2*(325 + 9*y**2 + 2*y**3) - 15*(-4199 + 3315*y + 650*y**2 + 182*y**3 + 45*y**4 + 7*y**5))/(3.93216e6*x**10)
+
+@jit("float64[:](float64)", nopython=True)
+def c_coefficients(x):
+    r"""Coefficients :math:`c_k(x)`.
+
+    The coefficients can be obtained from the series expansion
+
+    .. math::
+        \left(2x\frac{\cosh x - \cosh\left(\sqrt{x^2-y}\right)}{y \sinh(x)}\right)^{1/2} = \sum_{k=0}^\infty c_k(x) y^k\,.
+
+    The first coefficients are
+
+    .. math::
+        \begin{aligned}
+        c_0 &= 1\,, \\
+        c_1 &= \frac{1-x \coth (x)}{8 x^2}\,, \\
+        c_2 &= \frac{8 x^2-3 x^2 \coth ^2(x)-18 x \coth (x)+21}{384 x^4}\,, \\
+        c_3 &= \frac{-3 x^3 \coth ^3(x)+40 x^2-15 x^2 \coth ^2(x)-81 x \coth (x)+99}{3072 x^6}\,.
+        \end{aligned}
+
+    .. todo::
+        Explain cancellation problem and how to prevent it.
+
+    Parameters
+    ----------
+    x : float
+    
+    Returns
+    -------
+    nd.array
+        First six coefficients, :math:`\left[ c_0(x), \dots, c_5(x)\right]`.
+
+    """
+    c = np.empty(8)
+    c[0] = 1. 
+    c[1] = _c1(x)
+    c[2] = _c2(x)
+    c[3] = _c3(x)
+    c[4] = _c4(x)
+    c[5] = _c5(x)
+    return c 
+
+@jit("float64(float64, float64)", nopython=True)
+def pe_asymptotics(l, x):
+    r"""Exponentially scaled angular function :math:`\tilde p_\ell\left(\cosh(x)\right)` for large order.
+
+    The asymptotics of the angular function can be expressed through the
+    asymptotics of the associated Legendre function :math:`P^{-1}_\ell(\cosh x)`
+    and we find
+
+    .. math::
+        p_\ell\left(\cosh(x)\right) \simeq 2\left(\frac{x}{\sinh^3 x}\right)^{1/2} \sum_{k=0}^\infty c_k(x) \left(\frac{3}{2}\right)^{(k)} I_{k+1}(\nu x) \left(\frac{2x}{\nu}\right)^k
+    
+    with the coefficients :math:`c_k(x)` defined in :meth:`angular.c_coefficients` and
+    the rising factorial defined by
+    
+    .. math::
+        \left(\frac{3}{2}\right)^{(k)} = \frac{\Gamma(3/2+k)}{\Gamma(3/2)} 
+    
+    which is computed recursively. For the asymptotics :math:`\tilde p_\ell\left(\cosh(x)\right)`
+    one simply needs to replace the Bessel functions by their exponentially
+    scaled versions.
+        
+    Parameters
+    ----------
+    l : float
+        Order
+    x : float
+        Argument
+
+    Returns
+    -------
+    float
+        Exponentially scaled angular function
+
+    """
+    nu = l+0.5
+    kmax = 6
+    bessels = Ine(kmax+1, nu*x)
+    c = c_coefficients(x)
+
+    ff = 1.
+    ans = 0.
+    for k in range(kmax):
+        ans += c[k]*ff*bessels[k+1]*(2*x/nu)**k
+        ff *= 1.5+k
+    return 2.*np.sqrt(x/np.sinh(x)**3)*ans
+
+@jit("UniTuple(float64, 2)(float64, float64)", nopython=True)
+def pte_high(l, x):
+    r"""Exponentially scaled angular function :math:`\tilde p_\ell\left(\cosh(x)\right)`
+    and :math:`\tilde t_\ell\left(\cosh(x)\right)` for large order.
+
+    :math:`\tilde p_\ell\left(\cosh(x)\right)` is computed by calling :meth:`angular.pe_asymptotics`.
+    :math:`\tilde t_\ell\left(\cosh(x)\right)` is computed using the relation
+
+    .. math::
+        \tilde t_\ell(\cosh x) = -\cosh x\, \tilde p_\ell(\cosh x) + (2\ell+1) \tilde P_\ell(\cosh x)
+    
+    and :math:`\tilde P_\ell(\cosh x)` by calling :meth:`legendre.Ple_asymptotics`.
+    
+    Parameters
+    ----------
+    l : float
+        Order
+    x : float
+        Argument
+
+    Returns
+    -------
+    (float, float)
+        Exponentially scaled angular functions :math:`(\tilde p_\ell\left(\cosh(x)\right), \tilde t_\ell\left(\cosh(x)\right)`
+
+    """
+    pe = pe_asymptotics(l, x)
+    te = -np.cosh(x)*pe + (2*l+1)*Ple_asymptotics(l, x)
+    return pe, te
+
+@jit("UniTuple(float64[:], 2)(int64, float64)", nopython=True)
+def pte_low(l, x):
+    r"""Exponentially scaled angular function :math:`\tilde p_\ell\left(\cosh(x)\right)`
+    and :math:`\tilde t_\ell\left(\cosh(x)\right)` for small order.
+    
+    The angular functions are computed using the recurrence relation
+    
+    .. math::
+        \begin{aligned}
+        \tilde p_\ell(\cosh x) &= \frac{2\ell+1}{\ell+1}\left[\cosh x\, e^{-\vert x\vert} \tilde p_{\ell-1}(\cosh x) - \frac{\ell-2}{2\ell-3}e^{-2\vert x\vert}\tilde p_{\ell-2}(\cosh x)\right] \\
+        \tilde t_\ell(\cosh x) &= \ell \cosh x\, \tilde p_\ell(\cosh x) - \frac{(2\ell+1)(\ell-1)}{2\ell-1} e^{-\vert x\vert} \tilde p_{\ell-1}(\cosh x)
+        \end{aligned}
+
+    with the initial values :math:`\tilde p_0(\cosh x) = 0` and :math:`\tilde p_1(\cosh x) = \frac{3}{2} e^{\vert x\vert}`.
+    
+    .. todo::
+        Improve this.
+
+    Parameters
+    ----------
+    l : float
+        Order
+    x : float
+        Argument
+
+    Returns
+    -------
+    (nd.array, nd.array)
+        Exponentially scaled angular functions :math:`(\tilde p_\ell\left(\cosh(x)\right), \tilde t_\ell\left(\cosh(x)\right)`
+
+    """
+    p = np.empty(l)
+    t = np.empty(l)
+    z = np.cosh(x)
+    emz = np.exp(-x)
+    em2z = np.exp(-2*x)
+    p[-1] = 0.
+    p[0] = 3/2*np.exp(-1.5*x)
+    t[0] = z*p[0]
+    
+    for i in range(1, l):
+        p[i] = (2*i+3)/(i+2)*(z*emz*p[i-1] - (i-1)/(2*i-1)*em2z*p[i-2])
+        t[i] = (i+1)*z*p[i] - (2*i+3)*i/(2*i+1)*emz*p[i-1]
+    return p, t
+
+@jit("UniTuple(float64, 2)(int64, float64, float64[:,:])", nopython=True)
+def pte(l, x, cache):
+    if l < 1001:
+        return cache[0, l-1], cache[1, l-1]
+    else:
+        return pte_high(l, x)
+
+@jit("UniTuple(float64, 2)(int64, float64)", nopython=True)
+def pte_asymptotics(l, x):
+    if l < 1001:
+        pe, te = pte_low(l, x)
+        return pe[-1], te[-1]
+    else:
+        return pte_high(l, x)
---
+++ ufuncs-sphere/tests/test_angular.py
@@ -0,0 +1,99 @@
+import numpy as np
+import sys
+sys.path.append(".")
+import os
+from angular import pte_asymptotics
+
+from mpmath import *
+mp.dps = 80
+maxterms = 1e6
+def mp_pte(l, x):
+    z = cosh(x)
+    pe = (2*l+1)/(l*(l+1))*legenp(l, 1, z, type=3, maxterms=maxterms)/sinh(x)*exp(-(l+1/2)*x) 
+    te = -z*pe + (2*l+1)*legendre(l, z, maxterms=maxterms)*exp(-(l+1/2)*x)
+    return pe, te
+
+rtol=1.0e-15
+
+def test_pte_low():
+    lValues = np.floor(np.logspace(3.003, 9.08, 11))
+    for l in lValues:
+        xmax = np.arcsinh(25/(l+1))
+        X = np.empty(3)
+        # create random data between 0. and xmax
+        X = np.random.random(3)*xmax
+        for x in X:
+            mp_pe, mp_te = mp_pte(mpf(l), mpf(x))
+            num_pe, num_te = pte_asymptotics(l, x)
+            print("l", l, "x", x)
+            print("pe prec","%.16e"%(num_pe/mp_pe-1.))
+            print("pe","%.16e"%num_pe)
+            print("mp","%.16e"%mp_pe)
+            print("te prec","%.16e"%(num_te/mp_te-1.))
+            print("te","%.16e"%num_te)
+            print("mp","%.16e"%mp_te)
+            np.testing.assert_allclose(num_pe, float(mp_pe), rtol=rtol)
+            np.testing.assert_allclose(num_te, float(mp_te), rtol=rtol)
+
+rtol = 1e-15
+def test_pte_high():
+    InuKnue_data = np.loadtxt("tests/testdata/pte_high.dat")
+    for data in InuKnue_data:
+        num_pe, num_te = pte_asymptotics(data[0], data[1])
+        print("l", data[0], "x", data[1])
+        print(np.abs(num_pe/data[2]-1.))
+        print(np.abs(num_te/data[3]-1.))
+        np.testing.assert_allclose(num_pe, data[2], rtol=rtol)
+        np.testing.assert_allclose(num_te, data[3], rtol=rtol)
+
+def mp_c1(x):
+    return (1 - x*coth(x))/(8.*x**2)
+
+def mp_c2(x):
+    return (21 + 8*x**2 - 18*x*coth(x) - 3*x**2*coth(x)**2)/(384.*x**4)
+
+def mp_c3(x):
+    return (99 + 40*x**2 - 81*x*coth(x) - 15*x**2*coth(x)**2 - 3*x**3*coth(x)**3)/(3072.*x**6) 
+
+def mp_c4(x):
+    return (32175 + 13200*x**2 + 64*x**4 - 25740*x*coth(x) + 30*x**2*(-165 + 8*x**2)*coth(x)**2 - 1260*x**3*coth(x)**3 - 225*x**4*coth(x)**4)/(1.47456e6*x**8) 
+
+def mp_c5(x):
+    return (62985 + 26000*x**2 + 192*x**4 - x*(49725 + 64*x**4)*coth(x) + 30*x**2*(-325 + 24*x**2)*coth(x)**2 + 10*x**3*(-273 + 16*x**2)*coth(x)**3 - 675*x**4*coth(x)**4 - 105*x**5*coth(x)**5)/(3.93216e6*x**10) 
+
+rtol = 1.e-14
+from angular import _c1, _c2, _c3, _c4, _c5
+def test_c_coefficients():
+    X = np. logspace(-6, 3, 50)
+    for x in X:
+        print(x)
+        # allow less precision for higher coefficients since contribution decreases
+        np.testing.assert_allclose(_c1(x), float(mp_c1(mpf(x))), rtol=1.e-15)
+        np.testing.assert_allclose(_c2(x), float(mp_c2(mpf(x))), rtol=1.e-13)
+        np.testing.assert_allclose(_c3(x), float(mp_c3(mpf(x))), rtol=1.e-11)
+        np.testing.assert_allclose(_c4(x), float(mp_c4(mpf(x))), rtol=1.e-9)
+        np.testing.assert_allclose(_c5(x), float(mp_c5(mpf(x))), rtol=1.e-7)
+
+
+if __name__ == "__main__":
+    #test_pte_low()
+    #test_pte_high()
+    #test_c_coefficients()
+    #print(mp_c1(mpf(0.001)))
+    import matplotlib.pyplot as plt
+    x = np.arccosh(1.000001)
+    N = 40
+    L = np.floor(np.logspace(1, 4, 40))
+    pe = np.empty(N)
+    te = np.empty(N)
+    for i, l in enumerate(L):
+        print(i, l)
+        mp_pe, mp_te = mp_pte(mpf(l), mpf(x))
+        pe[i], te[i] = pte_asymptotics(l, x)
+        pe[i] = np.abs(pe[i]/mp_pe-1.)
+        te[i] = np.abs(te[i]/mp_te-1.)
+
+    f, ax = plt.subplots()
+    ax.loglog(L, pe)
+    ax.loglog(L, te)
+    plt.show()
