d60072a83b209572d7a1dadd3c3135c37e7f9f28
Benjamin <benjamin.spreng@physik.uni-augsburg.de>
2019-01-21 14:54:29 +0100

    alphabetic nomenclature

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ import sys
 # sys.path.insert(0, os.path.abspath('.'))
 sys.path.append('/home/benjamin/wd/nystrom/sphere')
 sys.path.append('/home/benjamin/wd/nystrom/ufuncs')
-sys.path.append('/home/benjamin/wd/nystrom/sphere-plane')
+sys.path.append('/home/benjamin/wd/nystrom/plane-sphere')
 
 # -- Project information -----------------------------------------------------
 
