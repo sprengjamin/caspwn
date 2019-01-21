e5cd757f6084538884ca6e0caf3e7c0dde720407
Benjamin <benjamin.spreng@physik.uni-augsburg.de>
2019-01-21 11:52:46 +0100

    add integrate methods

---
+++ ufuncs/integrate.py
@@ -0,0 +1,104 @@
+import numpy as np
+from numba import jit
+
+
+@jit("float64[:](int64)", nopython=True)
+def weights(N):
+    r"""Fourier-Chebyshev quadrature weights.
+    
+    """
+    i = np.arange(1, N+1, 1)
+    t = np.pi/(N+1)*i
+    wts = np.zeros(N)
+    for j in i:
+        wts += np.sin(j*t)*(1-np.cos(j*np.pi))/j
+    wts *= 2*np.sin(t)*(2/(N+1))/(1-np.cos(t))**2
+    return wts
+
+
+@jit("UniTuple(float64[:], 2)(int64)", nopython=True)
+def quadrature(N):
+    r"""Fourier-Chebyshev quadrature rule.
+
+    Parameters
+    ----------
+    N : int
+        quadrature order
+
+    Returns
+    -------
+    points : nd.array
+        quadrature points
+    weights : nd.array
+        quadrature weights
+    
+    """
+    i = np.arange(1, N+1, 1)
+    t = np.pi/(N+1)*i
+    pts = 1./(np.tan(t/2))**2
+    wts = weights(N)
+    return pts, wts
+
+
+def zipzap(a, b):
+    """ Zip two arrays together.
+    """
+    c = np.empty(len(a)+len(b))
+    c[::2] = a
+    c[1::2] = b
+    return c
+
+
+def auto_integrate(func, Ninit=10, rtol=1.0e-8):
+    r"""Automatic integration based on Fourier-Chebyshev scheme.
+
+    Determines the value of the integal
+        
+    .. math::
+        \int_0^\infty \mathrm{d}x\, f(x)\,.
+
+    
+    Parameters
+    ----------
+    func : function :math:`f`
+        integrand
+    Ninit : int
+        initial quadrature order
+    rtol : float
+        relative tolerance, precision of final result
+
+    Returns
+    -------
+    integral : float
+        integral
+    
+    """
+    N = Ninit
+    i = np.arange(1, N+1, 1)
+    t = np.pi/(N+1)*i
+    pts = 1./(np.tan(t/2))**2
+    
+    func_values = np.empty(N)
+    for i, p in enumerate(pts):
+        func_values[i] = func(p)
+    
+    wts = weights(N)
+     
+    integral = np.sum(func_values*wts)
+
+    while(True):
+        N = 2*N+1
+        i = np.arange(1, N+1, 2)
+        t = np.pi/(N+1)*i
+        pts = 1./(np.tan(t/2))**2
+    
+        func_values_new = np.empty(len(i))
+        for i, p in enumerate(pts):
+            func_values_new[i] = func(p)
+        func_values = zipzap(func_values_new, func_values)
+        wts = weights(N)
+        #print(N, integral)
+        integral_new = np.sum(func_values*wts)
+        if np.abs(integral/integral_new-1.) < rtol:
+            return integral_new
+        integral = integral_new
---
+++ ufuncs/tests/test_integrate.py
@@ -0,0 +1,63 @@
+import numpy as np
+import sys
+sys.path.append(".")
+from integrate import auto_integrate
+
+
+def test_function1():
+    function = lambda x: np.exp(-x)
+    
+    result = auto_integrate(function, Ninit=1, rtol=1.0e-15)
+    np.testing.assert_allclose(result, 1., rtol=1.0e-15)
+        
+    result = auto_integrate(function, Ninit=10, rtol=1.0e-10)
+    np.testing.assert_allclose(result, 1., rtol=1.0e-10)
+
+    result = auto_integrate(function, Ninit=5, rtol=1.0e-08)
+    np.testing.assert_allclose(result, 1., rtol=1.0e-08)
+
+
+def test_function2():
+    function = lambda x: 2/np.sqrt(np.pi)*np.exp(-x**2)
+    
+    result = auto_integrate(function, Ninit=1, rtol=1.0e-15)
+    np.testing.assert_allclose(result, 1., rtol=1.0e-15)
+        
+    result = auto_integrate(function, Ninit=10, rtol=1.0e-10)
+    np.testing.assert_allclose(result, 1., rtol=1.0e-10)
+
+    result = auto_integrate(function, Ninit=5, rtol=1.0e-08)
+    np.testing.assert_allclose(result, 1., rtol=1.0e-08)
+
+
+def test_function3():
+    function = lambda x: 1/(1+x)**2
+    
+    result = auto_integrate(function, Ninit=1, rtol=1.0e-15)
+    np.testing.assert_allclose(result, 1., rtol=1.0e-15)
+        
+    result = auto_integrate(function, Ninit=10, rtol=1.0e-10)
+    np.testing.assert_allclose(result, 1., rtol=1.0e-10)
+
+    result = auto_integrate(function, Ninit=5, rtol=1.0e-08)
+    np.testing.assert_allclose(result, 1., rtol=1.0e-08)
+
+
+def test_function4():
+    function = lambda x: 7/(1+x)**8
+    
+    result = auto_integrate(function, Ninit=1, rtol=1.0e-15)
+    np.testing.assert_allclose(result, 1., rtol=1.0e-15)
+        
+    result = auto_integrate(function, Ninit=10, rtol=1.0e-10)
+    np.testing.assert_allclose(result, 1., rtol=1.0e-10)
+
+    result = auto_integrate(function, Ninit=5, rtol=1.0e-08)
+    np.testing.assert_allclose(result, 1., rtol=1.0e-08)
+
+
+if __name__ == "__main__":
+    test_function1()
+    test_function2()
+    test_function3()
+    test_function4()
