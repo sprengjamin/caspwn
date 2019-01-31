73848dd55c302556b29b4054871cb255ff31ecdf
Benjamin <benjamin.spreng@physik.uni-augsburg.de>
2019-01-31 15:33:23 +0100

    adjust tool

--- analysis/PS1-Water-Silica1/compute_energy.py
+++ analysis/PS1-Water-Silica1/compute_energy.py
@@ -5,7 +5,7 @@ sys.path.append("../../sphere-sphere")
 sys.path.append("../../ufuncs")
 sys.path.append("../../sphere")
 sys.path.append("../../material")
-from energy import energy_finite, make_phiSequence
+from energy import energy_faster, make_phiSequence
 from kernel import phiKernel
 import energy
 energy.phiSequence = make_phiSequence(phiKernel)
@@ -16,10 +16,10 @@ Lvals = np.arange(0.008, 0.801, 0.001)[::-1]*1.e-06
 T = 293.015
 materials = ("PS1", "Water", "Silica1")
 
-eta = 5.
+eta = 10.
 nproc = 4
 
-filename = "energy_"+materials[0]+"_"+materials[1]+"_"+materials[2]+".dat"
+filename = "energy_"+materials[0]+"_"+materials[1]+"_"+materials[2]+"_v2.dat"
 
 if not os.path.isfile(filename):
     f=open(filename, "a")
@@ -30,7 +30,7 @@ if not os.path.isfile(filename):
         N = int(eta*np.sqrt(max(rho1, rho2)))
         M = N
         start = time.time()
-        en = energy_finite(R1, R2, L, T, materials, N, M, nproc)
+        en = energy_faster(R1, R2, L, T, materials, N, M, nproc)
         end = time.time()
         t = end-start
         f=open(filename, "ab")
