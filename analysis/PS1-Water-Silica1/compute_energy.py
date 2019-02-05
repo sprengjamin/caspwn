7d9544538329a2ac9f1fbce5ffc9b3e57abac96f
Benjamin <benjamin.spreng@physik.uni-augsburg.de>
2019-02-05 20:37:54 +0100

    adjust analysis

--- analysis/PS1-Water-Silica1/compute_energy.py
+++ analysis/PS1-Water-Silica1/compute_energy.py
@@ -19,7 +19,7 @@ materials = ("PS1", "Water", "Silica1")
 eta = 10.
 nproc = 10
 
-filename = "energy_"+materials[0]+"_"+materials[1]+"_"+materials[2]+"_v3.dat"
+filename = "energy_"+materials[0]+"_"+materials[1]+"_"+materials[2]+"_v4.dat"
 
 if not os.path.isfile(filename):
     f=open(filename, "a")
@@ -27,10 +27,12 @@ if not os.path.isfile(filename):
         print("computing L=",L)
         rho1 = R1/L
         rho2 = R2/L
-        N = int(eta*np.sqrt(max(rho1, rho2)))
-        M = N
+        rhoeff = rho1*rho2/(rho1+rho2)
+        Nout = int(eta*np.sqrt(rhoeff))
+        Nin = int(eta*np.sqrt(rho1+rho2))
+        M = Nin
         start = time.time()
-        en0, en = energy_faster(R1, R2, L, T, materials, N, M, nproc)
+        en0, en = energy_faster(R1, R2, L, T, materials, Nin, Nout, M, nproc)
         end = time.time()
         t = end-start
         f=open(filename, "ab")
