81f2a652781f1bebdb455cedfc539e27a3b38feb
Benjamin <benjamin.spreng@physik.uni-augsburg.de>
2019-02-19 14:09:47 +0100

    analysis of PS1-H2O-SiO2

--- analysis/PS1-Water-Silica1/force.py
+++ analysis/PS1-Water-Silica1/force.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 import sys
 sys.path.append("../../sphere-sphere")
 from PFA import PFA
-data0 = np.loadtxt("energy_PS1_Water_Silica1_v3.dat").T
-data1 = np.loadtxt("energy_PS1_Water_Silica1_v4.dat").T
+#data0 = np.loadtxt("energy_PS1_Water_Silica1_v6.dat").T
+data1 = np.loadtxt("energy_PS1_Water_Silica1_v6.dat").T
 #data2 = np.loadtxt("energy_PS1_Water_Silica1.dat").T
 pfa2 = np.loadtxt("pfa.dat")
-
 pfa = np.empty((2, len(data1[0])))
+"""
 # compute PFA
 for i, L in enumerate(data1[0]):
     R1 = 8.e-06
@@ -18,7 +18,7 @@ for i, L in enumerate(data1[0]):
     pfa[0, i], pfa[1, i] = PFA(R1, R2, L, T, materials)
 
 np.savetxt("my_pfa.dat", pfa)
-
+"""
 pfa = np.loadtxt("my_pfa.dat")
 
 R1 = 8.e-06
@@ -34,8 +34,9 @@ rel = np.fabs(pfa[0]/full_force-1.)
 rel2 = np.fabs(pfa[1]/full_force2-1.)
 
 f, ax = plt.subplots()
-ax.plot(data0[0], np.gradient(data0[2], 1.e-09))
-ax.plot(data1[0], np.gradient(data1[2], 1.e-09))
+#ax.plot(data0[0], np.gradient(data0[2], 1.e-09))
+ax.loglog(data1[0, 1:-1], rel[1:-1], label="with $n=0$")
+ax.loglog(data1[0, 1:-1], rel2[1:-1], label="without $n=0$")
 #ax.loglog(L*1e6, rel[1:-1], "b-",label="with n=0")
 #ax.loglog(L*1e6, rel2[1:-1], "g-",label="without")
 #ax.loglog(L*1e6, 0.1*L*1e6, "k-",label="power")
