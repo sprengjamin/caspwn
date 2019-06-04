782f6e0eae0744b5b3a583e418ba5da0e0acbbde
Benjamin <benjamin.spreng@physik.uni-augsburg.de>
2019-06-04 13:11:44 +0200

    adjust plane-plane energy to K=0-materialclass

--- plane-plane/energy.py
+++ plane-plane/energy.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numba import njit
+#from numba import njit
 from scipy.integrate import quad
 import sys, os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../plane/"))
@@ -11,21 +11,23 @@ from psd import psd
 
 from scipy.constants import Boltzmann, hbar, c
 
-def integrand(K, k, eps1, eps2):
+def integrand(K, k, eps1, matclass1, eps2, matclass2):
     kappa = np.sqrt(K**2 + k**2)
-    ans = np.log1p(-rTE(K, k, eps1)*rTE(K, k, eps2)*np.exp(-2*kappa))
-    ans += np.log1p(-rTM(K, k, eps1)*rTM(K, k, eps2)*np.exp(-2*kappa))
+    ans = np.log1p(-rTE(K, k, eps1, matclass1)*rTE(K, k, eps2, matclass2)*np.exp(-2*kappa))
+    ans += np.log1p(-rTM(K, k, eps1, matclass1)*rTM(K, k, eps2, matclass2)*np.exp(-2*kappa))
     return k/(2*np.pi)*ans
      
 
 def energy_finite(L, T, materials, epsrel=1.e-08):
     K_matsubara = Boltzmann*T/(hbar*c)
     eps_plane1 = eval("material."+materials[0]+".epsilon(0.)")
+    matclass1 = eval("material."+materials[0]+".materialclass")
     eps_medium = eval("material."+materials[1]+".epsilon(0.)")
     eps_plane2 = eval("material."+materials[2]+".epsilon(0.)")
+    matclass2 = eval("material."+materials[2]+".materialclass")
     eps1 = eps_plane1/eps_medium
     eps2 = eps_plane2/eps_medium
-    f = lambda k: integrand(0., k, eps1, eps2)
+    f = lambda k: integrand(0., k, eps1, matclass1, eps2, matclass2)
     energy0 = quad(f, 0, np.inf)[0]/L**2
     Teff = 4*np.pi*Boltzmann/hbar/c*T*L
     N = int(max(np.ceil((1-1.5*np.log10(np.abs(epsrel)))/np.sqrt(Teff)), 5))
@@ -37,7 +39,7 @@ def energy_finite(L, T, materials, epsrel=1.e-08):
         eps_plane2 = eval("material."+materials[2]+".epsilon(K_matsubara*xi[n])")
         eps1 = eps_plane1/eps_medium
         eps2 = eps_plane2/eps_medium
-        f = lambda k: integrand(K_matsubara*xi[n]*L*np.sqrt(eps_medium), k, eps1, eps2)
+        f = lambda k: integrand(K_matsubara*xi[n]*L*np.sqrt(eps_medium), k, eps1, matclass1, eps2, matclass2)
         term = quad(f, 0, np.inf)[0]/L**2
         energy += 2*eta[n]*term
     return 0.5*T*Boltzmann*(energy0+energy), 0.5*T*Boltzmann*energy
@@ -49,5 +51,4 @@ if __name__ == "__main__":
     T = 293.015
     materials = ("PS1", "Water", "Silica1")
     print(energy_finite(L, T, materials))
-    print(energy_faster(L, T, materials))
 
