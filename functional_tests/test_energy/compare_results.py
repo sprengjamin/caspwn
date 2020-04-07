60291064144564dd3b84fd5d9f6d34466fbfd319
Benjamin <benjamin.spreng@physik.uni-augsburg.de>
2020-04-07 17:56:56 +0200

    Fix functional tests
    Due to a few changes the functional tests did not work anymore. This commit fixes those.

--- functional_tests/test_energy/compare_results.py
+++ functional_tests/test_energy/compare_results.py
@@ -2,21 +2,9 @@ import numpy as np
 from scipy.constants import hbar, c
 
 for i in range(1, 21):
-    if i > 4 and i < 9:
-        continue
     I = "%02d" % (i,)
     ref = np.loadtxt("reference_data/nystrom/data"+I+".out")
     new = np.loadtxt("test_data/data"+I+".out")
-    if i > 8 and i < 13:
-        ref *= hbar*c
-        if i == 9:
-            ref /= 1.e-06
-        elif i == 10:
-            ref /= 0.25e-06
-        elif i == 11:
-            ref /= 3.e-06
-        elif i == 12:
-            ref /= 0.75e-06
     np.testing.assert_allclose(new, ref, rtol=1e-06, err_msg="Problem occured in data"+I+".out")
 
 for i, j in enumerate(range(21, 25)):
