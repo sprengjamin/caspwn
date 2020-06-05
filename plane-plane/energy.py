cdc180d70e315c2e837a9a698038f5d829e655c0
Benjamin <benjamin.spreng@physik.uni-augsburg.de>
2020-06-05 11:37:47 +0200

    adjust plane-plane energy to changes in fresnel coefficients, make output like plsp and spsp

--- plane-plane/energy.py
+++ plane-plane/energy.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.integrate import quad
 import sys, os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../plane/"))
-from fresnel import rTE, rTM
+from fresnel import rTE_zero, rTE_finite, rTM_zero, rTM_finite
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../material/"))
 import material
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../ufuncs/"))
@@ -11,23 +11,41 @@ from psd import psd
 
 from scipy.constants import Boltzmann, hbar, c
 
-def integrand(K, k, eps1, matclass1, eps2, matclass2):
+def integrand_zero(k, eps1, matclass1, eps2, matclass2):
+    ans = np.log1p(-rTE_zero(k, eps1, matclass1)*rTE_zero(k, eps2, matclass2)*np.exp(-2*k))
+    ans += np.log1p(-rTM_zero(k, eps1, matclass1)*rTM_zero(k, eps2, matclass2)*np.exp(-2*k))
+    return k/(2*np.pi)*ans
+
+def integrand_finite(K, k, eps1, eps2):
     kappa = np.sqrt(K**2 + k**2)
-    ans = np.log1p(-rTE(K, k, eps1, matclass1)*rTE(K, k, eps2, matclass2)*np.exp(-2*kappa))
-    ans += np.log1p(-rTM(K, k, eps1, matclass1)*rTM(K, k, eps2, matclass2)*np.exp(-2*kappa))
+    ans = np.log1p(-rTE_finite(K, k, eps1)*rTE_finite(K, k, eps2)*np.exp(-2*kappa))
+    ans += np.log1p(-rTM_finite(K, k, eps1)*rTM_finite(K, k, eps2)*np.exp(-2*kappa))
     return k/(2*np.pi)*ans
      
 
 def energy_finite(L, T, materials, mode, epsrel=1.e-08, N=None):
     K_matsubara = Boltzmann*T/(hbar*c)
-    eps_plane1 = eval("material."+materials[0]+".epsilon(0.)")
-    matclass1 = eval("material."+materials[0]+".materialclass")
     eps_medium = eval("material."+materials[1]+".epsilon(0.)")
-    eps_plane2 = eval("material."+materials[2]+".epsilon(0.)")
+    
+    matclass1 = eval("material."+materials[0]+".materialclass")
+    if matclass1 == "dielectric":
+        eps_plane1 = eval("material."+materials[0]+".epsilon(0.)")
+        eps1 = eps_plane1/eps_medium
+    elif matclass1 == "PR":
+        eps1 = 1.
+    else:
+        eps1 = eval("material."+materials[0]+".K_plasma")*L
+
     matclass2 = eval("material."+materials[2]+".materialclass")
-    eps1 = eps_plane1/eps_medium
-    eps2 = eps_plane2/eps_medium
-    f = lambda k: integrand(0., k, eps1, matclass1, eps2, matclass2)
+    if matclass2 == "dielectric":
+        eps_plane2 = eval("material."+materials[2]+".epsilon(0.)")
+        eps2 = eps_plane2/eps_medium
+    elif matclass2 == "PR":
+        eps2 = 1.
+    else:
+        eps2 = eval("material."+materials[2]+".K_plasma")*L
+    
+    f = lambda k: integrand_zero(k, eps1, matclass1, eps2, matclass2)
     energy0 = quad(f, 0, np.inf)[0]/L**2
     energy = 0.
     if mode == "psd":
@@ -42,7 +60,7 @@ def energy_finite(L, T, materials, mode, epsrel=1.e-08, N=None):
             eps_plane2 = eval("material."+materials[2]+".epsilon(K_matsubara*xi[n])")
             eps1 = eps_plane1/eps_medium
             eps2 = eps_plane2/eps_medium
-            f = lambda k: integrand(K_matsubara*xi[n]*L*np.sqrt(eps_medium), k, eps1, matclass1, eps2, matclass2)
+            f = lambda k: integrand_finite(K_matsubara*xi[n]*L*np.sqrt(eps_medium), k, eps1, eps2)
             term = quad(f, 0, np.inf)[0]/L**2
             energy += 2*eta[n]*term
     elif mode == "msd":
@@ -56,24 +74,26 @@ def energy_finite(L, T, materials, mode, epsrel=1.e-08, N=None):
             eps_plane2 = eval("material."+materials[2]+".epsilon(2*np.pi*K_matsubara*n)")
             eps1 = eps_plane1/eps_medium
             eps2 = eps_plane2/eps_medium
-            f = lambda k: integrand(2*np.pi*K_matsubara*n*L*np.sqrt(eps_medium), k, eps1, matclass1, eps2, matclass2)
+            f = lambda k: integrand_finite(2*np.pi*K_matsubara*n*L*np.sqrt(eps_medium), k, eps1, eps2)
             term = quad(f, 0, np.inf)[0]/L**2
-            print(n, term)
+            #print(n, term)
             energy += 2*term
-            if abs(term/energy) < epsrel:
-                break
+            if N == np.inf:
+                if abs(term/energy) < epsrel:
+                    print("msd nmax", n)
+                    break
             n += 1
     else:
         assert(True)
-    return 0.5*T*Boltzmann*(energy0+energy), 0.5*T*Boltzmann*energy
+    return 0.5*T*Boltzmann*energy0, 0.5*T*Boltzmann*energy
     
 
 
 if __name__ == "__main__":
     L = 0.01e-06
-    T = 293.015
-    materials = ("PS1", "Water", "PS1")
-    materials = ("PR", "Vacuum", "PR")
-    print(energy_finite(L, T, materials, "msd"))
+    T = 298.015
+    materials = ("PS1", "WaterRT", "PS1")
+    #materials = ("PR", "Vacuum", "PR")
+    print(energy_finite(L, T, materials, "msd", N=700))
     print(energy_finite(L, T, materials, "psd"))
 
