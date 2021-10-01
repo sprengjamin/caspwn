54b5c4360097cba06469936f5211d60f61080c9a
Benjamin <benjamin.spreng@physik.uni-augsburg.de>
2021-10-01 14:55:12 -0700

    update functional tests

--- functional_tests/test_energy/compare_results.py
+++ functional_tests/test_energy/compare_results.py
@@ -4,11 +4,11 @@ from scipy.constants import hbar, c
 for i in range(1, 21):
     I = "%02d" % (i,)
     ref = np.loadtxt("reference_data/nystrom/data"+I+".out")
-    new = np.loadtxt("test_data/data"+I+".out")
-    try:
-        new[0] = new[0]+new[1]
-    except IndexError:
-        pass
+    res = np.loadtxt("test_data/data"+I+".out")
+    try: # need to do this due to old output format of reference data
+        new = np.array([np.sum(res), res[2]])
+    except IndexError: # this catches the T=0 case
+        new = res
 
     np.testing.assert_allclose(new, ref, rtol=1e-06, err_msg="Problem occured in data"+I+".out")
 
@@ -19,7 +19,7 @@ for i, j in enumerate(range(21, 25)):
     R = ref[2]
     E_ref = ref[5]*hbar*c/(L+R)
     new = np.loadtxt("test_data/data"+str(j)+".out")
-    E_new = new[0]+new[1]
+    E_new = np.sum(new)
     np.testing.assert_allclose(E_new, E_ref, rtol=5e-06, err_msg="Problem occured in data"+str(j)+".out")
 
 print("All tests successful!")
